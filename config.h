2c900609121fb5d0eaf441e1a2d56cffaad6f962
Mizari <gomizari@gmail.com>
2017-08-09 11:56:37 +0700

    forgotten files for previous commit

---
+++ config.h
@@ -0,0 +1,12 @@
+
+#ifndef _CONFIG
+
+#define _CONFIG
+
+#define MAX_CLIENTS 10
+#define MAX_SEATS   30
+#define MAX_THREADS 10
+#define MAX_ROLES   50
+#define PORT        1234
+
+#endif
---
+++ main.h
@@ -0,0 +1,23 @@
+#include "config.h"
+
+typedef struct {
+	int connection;
+	int seat;
+	int ticket;
+	int id;
+	char role[MAX_ROLES];
+} client;
+
+typedef struct {
+    client all_clnts[MAX_CLIENTS];
+    int count_of_clnts;
+    int seats[MAX_SEATS];
+    int dvigatel;
+    pthread_t id_of_threads[MAX_THREADS];
+    pthread_mutex_t mutex_seat;
+} marshrutka_t;
+
+typedef struct {
+    marshrutka_t *bus;
+    int i;
+} thread_arg;
