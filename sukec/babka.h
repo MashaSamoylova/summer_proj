ce423a216e4f857e512b135eb021d059adf1df49
MashaSamoylova <mashanedyak@gmail.com>
2017-09-25 22:48:17 +0700

    я сомневаюсь, что это так работает

--- sukec/babka.h
+++ sukec/babka.h
@@ -1,74 +1,8 @@
-#pragma once
 #include "main.h"
 #include "client.h"
 
-void generate_open(struct client_t* Katya) {
-   //нужно решить как генерировать события, если id пассажиров 0 1 2 , 1 уходит, то здесь с сегфолтом упадет через 
-    int number = rand()%Katya->bus->n_passzh;
-    printf("generate_open number %d id babi %d\n", number, Katya->id);
-    generate_event(number, 1, Katya);
-}
-
-int babka_generator(struct client_t* Katya){
-    
-    //window is open
-    if(Katya->bus->status_window) {
-        generate_open(Katya);
-    }
-    else{
-        //generate_close(Katya);
-    }
-    return 0;
-}
-
-int baba_handler(struct client_t* Katya) {
-    
-    struct babka *Klava = (struct babka*)Katya;
-    
-    while( !empty(Katya) ) {
-        int code = Katya->first_event->code;
-        printf("id %d\n", Katya->id);
-        /*и здесь также добавить обработчики на события*/
-        switch(code) {
-            case 101:
-                printf("event 101\n");
-                break;
-            case 102:
-                printf("event 102\n");
-                break;
-            default:
-                break;
-        }
-        delete_event(Katya);
-        printf("event is handled\n");
-    }
-
-    
-    printf("baba action %d\n", Katya->id);
-    return 0;
-}
-
-void add_babka(marshrutka_t* bus) {
-    struct babka *Katya = calloc(1, sizeof(struct babka));
-    struct client_t *client = (struct client_t*)Katya;
-
-    Katya->client.bus = bus;
-    insert_client(client);
-
-    Katya->hp = 100;
-    Katya->client.handler = baba_handler;
-    Katya->client.generator = babka_generator;
-    bus->n_babok++;
-}
-
-void delete_babka(struct babka* Katya) {
-    struct client_t *Klava = (struct client_t*)Katya;
-    Klava->bus->n_babok--;
-    delete_client(Klava);
-}
-
-/*void generate_close( struct babka* Katya, struct client* Ivan ) {
-    generate_event(Ivan, 2, Katya->id);    
-}
-
-*/
+void generate_open(struct client_t* Katya);
+int babka_generator(struct client_t* Katya);
+int baba_handler(struct client_t* Katya);
+void add_babka(marshrutka_t* bus);
+void delete_babka(struct babka* Katya);
---
+++ sukec/client.c
@@ -0,0 +1,128 @@
+#include "client.h"
+
+int clean_events(struct client_t *Ivan) {
+    printf("чистка событий\n");
+    struct event *arrow = Ivan->first_event;
+    struct event *cup;
+
+    while(arrow) {
+        cup = arrow;
+        arrow = arrow->next_event;
+        free(cup);
+    }
+    return 0;
+}
+
+int empty(struct client_t *Ivan) {
+    
+    if(Ivan->first_event == NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+int delete_client(struct client_t * Ivan) {
+    clean_events(Ivan);
+    free(Ivan->context.uc_stack.ss_sp);
+    Ivan->prev_client->next_client = Ivan->next_client;
+    struct client_t* cup = Ivan->next_client;
+    Ivan->bus->n_clients--;
+    free(Ivan);
+    setcontext(&cup->context);
+    return 0;
+}
+
+void add_event(struct client_t *Ivan, struct event *sc) {
+    
+    if( empty(Ivan) ) { //queue is empty
+        Ivan->first_event = sc;
+        return;
+    }
+
+    struct event* last = Ivan->first_event;
+    while(last->next_event != NULL) {
+        last = last->next_event;
+    }
+    last = sc;
+}
+
+void delete_event(struct client_t *Ivan) {
+    struct event* cup = Ivan->first_event;
+    Ivan->first_event = Ivan->first_event->next_event;
+    free(cup);
+}
+
+void generate_event(int number, int code, struct client_t *Ivan) {
+    
+    struct client_t *arrow = Ivan->bus->first_client;
+   
+    //поиск клиента, которому хотят добавить событие
+    while(number) {
+        arrow = arrow->next_client;
+        number--;
+    }
+
+    struct event *sc = calloc(1, sizeof(struct event));
+    sc->code = code;
+    sc->ask_id = Ivan->id;
+    sc->next_event = NULL;
+
+    add_event(arrow, sc);
+}
+
+int loop(struct client_t* Ivan) {
+    while(1) {
+        if(Ivan->bus->n_passzh == 0) {
+            sleep(10);
+            return 0;
+        }
+        printf("client id %d\n", Ivan->id);
+        
+        if(!empty(Ivan)) {
+            Ivan->handler(Ivan);
+        }
+        
+        Ivan->generator(Ivan);
+        swapcontext(&Ivan->context, &Ivan->next_client->context);
+    }
+}
+
+int init_context(struct client_t *Ivan) {
+    Ivan->context.uc_link = NULL;
+    Ivan->context.uc_stack.ss_sp = calloc(SIGSTKSZ, sizeof(char));
+    Ivan->context.uc_stack.ss_size = SIGSTKSZ * sizeof(char);
+
+    getcontext(&Ivan->context);
+    makecontext(&Ivan->context, (void (*)(void))loop,
+        1, Ivan);
+    return 0;
+
+}
+
+int insert_client(struct client_t* Ivan) {
+    
+    if(!Ivan->bus) {
+        printf("клиент без автобуса\n");
+        return -1;
+    }
+
+    Ivan->id = rand()%1000;
+    printf("%d\n", Ivan->id);
+    Ivan->first_event = NULL;
+    Ivan->last_event = NULL;
+
+    if(Ivan->bus->first_client == NULL) {
+        Ivan->bus->first_client = Ivan; 
+        Ivan->bus->last_client = Ivan;
+    }
+
+    Ivan->bus->last_client->next_client = Ivan;
+    Ivan->prev_client = Ivan->bus->last_client;
+    Ivan->bus->last_client = Ivan;
+    Ivan->next_client = Ivan->bus->first_client;
+   
+    init_context(Ivan);
+    Ivan->bus->n_clients++;
+    return 0;
+}
+
