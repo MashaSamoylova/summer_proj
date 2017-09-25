126cb856a34f6fd0cda296bda2e10fc813f8fc74
MashaSamoylova <mashanedyak@gmail.com>
2017-09-25 23:17:00 +0700

    сделали раздельную компиляцию модулей

--- sukec/marshrutka.h
+++ sukec/marshrutka.h
@@ -1,67 +1,9 @@
-#pragma once
+#include <stdio.h> 
+#include <stdlib.h>
+#include <arpa/inet.h>
+
 #include "main.h"
-//*очистка памяти*/
 
-void scrabwoman(marshrutka_t* bus) {
-
-    int k = 0;
-    struct client_t* arrow = bus->first_client;
-    struct client_t* cup;
-    while(k < bus->n_clients) {
-        cup = arrow;
-        clean_events(arrow);
-        free(arrow->context.uc_stack.ss_sp);
-        arrow = arrow->next_client;
-        free(cup);
-        k++;
-    }
-    free(bus);
-}
-
-/*возвращает сокет для всего сервиса*/
-int zavesti_marshrutku() {
-    /*
-#ifndef PRAVILNIE_ZAPCHASTI
-    return 0;
-#endif
-*/
-
-    struct sockaddr_in server;
-    int dvigatel = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (dvigatel == -1) {
-        fprintf(stderr, "Could not create socket\n");
-        return 0; 
-    }
-
-    int yes = 1;
-    setsockopt(dvigatel, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(PORT);
-
-    if (bind(dvigatel, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        fprintf(stderr, "ERROR on binding\n");
-        return 0;
-    }
-    listen(dvigatel, 5);
-    return dvigatel;
-}
-
-/*void broadcast(marshrutka_t *bus, char* message) {
-    struct client* arrow = bus->first_client;
-
-    do {
-        write(arrow->connection, message, strlen(message));
-        arrow = arrow->next_client;
-    } while(arrow != bus->first_client);
-
-}*/
-
-void* otpravit_marshrutku(marshrutka_t *bus) {
-    printf("поееехааали\n");
-    loop(bus->first_client);
-	return NULL;
-}
-
+void scrabwoman(marshrutka_t* bus);
+int zavesti_marshrutku();
+void* otpravit_marshrutku(marshrutka_t *bus);
--- sukec/passazhir.h
+++ sukec/passazhir.h
@@ -1,182 +1,23 @@
-#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #include "main.h"
 #include "client.h"
 
-/*отключение всех клиентов*/
-void vypnut_passazhirov(marshrutka_t *bus) {
-    struct client_t* arrow = bus->first_client;
-    int k = 0;
-    
-    while(k < bus->n_clients) {
-        struct passazhir *Tom = (struct passazhir*)arrow;
-        close(Tom->ufds.fd);
-        arrow = arrow->next_client;
-        k++;
-    }
-}
+void vypnut_passazhirov(marshrutka_t *bus);
+void delete_passazh(struct passazhir* Tom);
+int detect_disconnect(struct passazhir* Tom);
+int try_write(struct passazhir* Tom, char* message);
+int write_passazh(struct passazhir *Tom, char* message);
+void hello(struct passazhir* Tom);
+int try_read_answer(struct passazhir* Tom, char *buffer, int size);
+int read_answer(struct passazhir* Tom, char *buffer, int size);
+int open_window(struct passazhir *Tom);
+int passazhir_handler(struct client_t* Ivan);
+int passazhir_generator(struct client_t* Ivan);
+int new_client(int dvigatel);
+int add_passzhir(marshrutka_t* bus);
 
-void delete_passazh(struct passazhir* Tom) {
-    struct client_t *Ivan = (struct client_t*)Tom;
-    Ivan->bus->n_passzh--;
-    delete_client(Ivan);
-}
-
-int detect_disconnect(struct passazhir* Tom) {
-    Tom->ufds.events = POLLOUT;
-    return poll(&Tom->ufds, 1, 10000);
-}
-
-int try_write(struct passazhir* Tom, char* message) {
-    Tom->ufds.events = POLLOUT;
-    int n = poll(&Tom->ufds, 1, 10000);
-
-    if (n < 0) {
-        return -1; //poll error
-    }
-
-    if (n == 0) {
-        return 1;
-    }
-
-    if(Tom->ufds.revents == POLLOUT) {
-        n = send(Tom->ufds.fd, message, strlen(message), 0);
-        if(n <= 0) return -1;
-        return n;
-    }
-    return -1;
-}
-
-int write_passazh(struct passazhir *Tom, char* message) {
-    int k = 3;
-    while(k > 0) {
-        if(try_write(Tom, message) != -1)
-            return 0;
-       k--;
-    }
-    return -1;        
-}
-
-void hello(struct passazhir* Tom) {
-    char buff[256];
-    char p[1000];
-	*p = 0;
-    FILE *bus_ascii = fopen("bus", "r");
-    if(bus_ascii == NULL) {
-        printf("not found the bus:(\n");
-        exit(1);
-    }
-
-    while(fgets(buff, 256, bus_ascii)) {
-        strncat(p, buff, strlen(buff));
-    }
-    write_passazh(Tom, p);
-    fclose(bus_ascii);
-
-    char msg[40] = "\nМАРШРУТКА № 442\n";
-    write_passazh(Tom, msg);
-    return;	
-}
-
-int try_read_answer(struct passazhir* Tom, char *buffer, int size) {
-    Tom->ufds.events = POLLIN;
-    int n = poll(&Tom->ufds, 1, 10000);
-    if(n < 0) {
-        return -1; //poll error
-    }
-
-    if(n == 0) {
-        return 0; //cant read yet
-    }
-
-    if(Tom->ufds.revents == POLLIN) {
-        n = recv(Tom->ufds. fd, buffer, size, 0);
-        if(n <= 0) return -1;
-        return n;
-    }
-
-    return -1;
-}
-
-int read_answer(struct passazhir* Tom, char *buffer, int size) {
-    int k = 3;
-    while(k > 0) { 
-    if(try_read_answer(Tom, buffer, size) != -1)
-            return 0;
-       k--;
-    }
-    return -1; 
-}
-
-int open_window(struct passazhir *Tom) {
-    char buff[100] = "\0";
-    write_passazh(Tom, "1.открыть окно\n2.ударить бабку\n");
-    read_answer(Tom, buff, 10);
-    printf("%s\n", buff);
-    return 0;
-}
-
-int passazhir_handler(struct client_t* Ivan) {
-
-    struct passazhir *Tom = (struct passazhir*)Ivan;
-   
-    if(detect_disconnect(Tom) == -1) {
-        delete_passazh(Tom);
-    }
-    
-    while( !empty(Ivan) ) {
-        int code = Ivan->first_event->code;
-        printf("id %d\n", Ivan->id);
-        /*здесь нужно добавить обработчики на события, 
-         * типо для кода 1, 2 и тд*/
-        switch(code) {
-            case 1:
-                open_window(Tom);
-                break;
-            case 2:
-                printf("event 2\n");
-                break;
-            default:
-                break;
-        }
-        delete_event(Ivan);
-        printf("event is handled\n");
-    }
-
-    return 0;
-}
-
-int passazhir_generator(struct client_t* Ivan) {
-    struct passazhir *Tom = (struct passazhir*)Ivan;
-    printf("generate TOM %d\n", Tom->client.id);
-    return 0;
-}
-
-int new_client(int dvigatel) {
-
-    struct sockaddr_in cli_addr;
-    int newsockfd;
-    socklen_t client_ss;
-
-    client_ss = sizeof(cli_addr);
-    newsockfd = accept(dvigatel, (struct sockaddr *) &cli_addr, &client_ss);
-    if (newsockfd < 0) {
-        printf("ERROR on accept\n");
-    }
-    return newsockfd;
-}
-
-int add_passzhir(marshrutka_t* bus) {
-
-    struct passazhir *Ivan = calloc(1, sizeof(struct passazhir));
-    struct client_t* client = (struct client_t*)Ivan;
-
-    Ivan->client.bus = bus;
-    insert_client(client);
-    
-    Ivan->ufds.fd = new_client(bus->dvigatel);
-    Ivan->client.handler = passazhir_handler;
-    Ivan->client.generator = passazhir_generator;
-    bus->n_passzh++;
-    hello(Ivan);
-    return 0;
-}
