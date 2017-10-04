ee639885ddabd5c4a7dfce2dfc0ff441b32b12f6
MashaSamoylova <mashanedyak@gmail.com>
2017-10-04 22:57:35 +0700

    перенесла zavesty_marshrutku, потому что кажется логичнее

--- sukec/marshrutka.c
+++ sukec/marshrutka.c
@@ -17,37 +17,6 @@ void scrabwoman(marshrutka_t* bus) {
     free(bus);
 }
 
-/*возвращает сокет для всего сервиса*/
-int zavesti_marshrutku() {
-    /*
-#ifndef PRAVILNIE_ZAPCHASTI
-    return 0;
-#endif
-*/
-
-    struct sockaddr_in server;
-    int dvigatel = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (dvigatel == -1) {
-        fprintf(stderr, "Could not create socket\n");
-        return 0; 
-    }
-
-    int yes = 1;
-    setsockopt(dvigatel, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(PORT);
-
-    if (bind(dvigatel, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        fprintf(stderr, "ERROR on binding\n");
-        return 0;
-    }
-    listen(dvigatel, 5);
-    return dvigatel;
-}
-
 /*void broadcast(marshrutka_t *bus, char* message) {
     struct client* arrow = bus->first_client;
 
